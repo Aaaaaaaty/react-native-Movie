e987ce5332f45fbf671d9efbcd3a3ac5d9950b9e
WangJie <wangjie111619@pwrd.com>
2017-04-13 15:37:29 +0800

    初始化reactnative-redux

---
+++ js/App.js
@@ -0,0 +1,24 @@
+
+'use strict';
+
+import {Provider} from 'react-redux';
+import React from 'react';
+import {Navigator} from 'react-native';
+import store from './Redux/Store/Store';
+import UserInfo from './Container/UserInfor';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <Provider store={store}>
+                <Navigator
+                    initialRoute={{component: UserInfo}}
+                    renderScene={(route, navigator) =>
+                        <route.component {...route.args} navigator={navigator} />
+                    }
+                />
+            </Provider>
+        )
+    }
+}
