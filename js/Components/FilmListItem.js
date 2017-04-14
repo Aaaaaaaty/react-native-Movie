332255a03074dfb25ef8d85c938dfc31ed71fad6
An <2103887953@qq.com>
2017-04-14 18:21:01 +0800

    textoverflow cant sup

--- js/Components/FilmListItem.js
+++ js/Components/FilmListItem.js
@@ -1,5 +1,5 @@
 import React, {Component, PropTypes} from "react"
-import {StyleSheet, View, Image, Text, PixelRatio, Dimensions} from "react-native"
+import {StyleSheet, View, Image, Text, PixelRatio, Dimensions, ScrollView} from "react-native"
 import pxTodp from '../utils/pxTodp'
 
 export default class filmListItem extends Component {
@@ -20,39 +20,57 @@ export default class filmListItem extends Component {
               style={ styles.filmListItem }>
           <View>
             <Image  source={ posterUrl }
-                    style={{width: 40, height: 40}}/>
+                    style={styles.image}/>
             </View>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}>
+          <View style={ styles.message }>
             <Text>{ item.name }</Text>
+            <Text style={ styles.messageAvtor }>
+              <Text>{ item.phrase }</Text>
+              <Text>{ item.starring }</Text>
+            </Text>
+            <Text>2D</Text>
           </View>
-          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}>
+          <View style={ styles.btn }>
             <Text>购票</Text>
           </View>
         </View>
       )
     })
       return (
-        <View style={ styles.filmListWrapper }>
+        <ScrollView style={ styles.filmListWrapper }>
           { filmListItem }
-        </View>
+        </ScrollView>
       )
   }
 }
 
 const styles = StyleSheet.create({
   filmListWrapper: {
-      width: pxTodp(400),
-      // margin: 50,
       flexDirection: 'column',
-      // justifyContent: 'space-between',
   },
   filmListItem: {
-      // width:pxTodp(750),
-      backgroundColor: 'green',
-      // height: 150,
-      // margin: 50,
-      // flex: 1,
+      marginBottom: pxTodp(50),
       flexDirection: 'row',
-      // justifyContent: 'space-around',
+      justifyContent: 'space-between',
+  },
+  image: {
+    width: pxTodp(150),
+    height: pxTodp(200)
+  },
+  message: {
+    flex: 1,
+    backgroundColor: 'gray',
+    marginLeft: pxTodp(20)
+  },
+  messageAvtor: {
+    marginTop: pxTodp(20),
+    marginBottom: pxTodp(20),
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  },
+  btn: {
+    width: pxTodp(100),
+    backgroundColor: 'yellow'
   }
 })
