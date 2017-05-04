9a266852baf8e8b31eec2679fe225cc2da68d168
An <2103887953@qq.com>
2017-05-04 18:18:49 +0800

    add filmSchemaList

---
+++ js/Components/FilmSchemaList.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react'
+import { mapStateToProps, mapDispatchToProps } from '../Redux/Store/Store'
+import { connect, Provider } from 'react-redux'
+import { StyleSheet, View, Image, Text, WebView, TouchableOpacity, ScrollView } from "react-native"
+import pxTodp from '../utils/pxTodp'
+
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+class FilmMe extends Component {
+	static navigationOptions = {
+    tabBarLabel: '我的',
+  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			listType: 'online'
+		}
+	}
+	componentWillMount() {
+    let { filmTimeList } = this.props
+    this.setState({
+      filmTimeList: filmTimeList
+    })
+  }
+	componentDidMount() {}
+  componentWillReceiveProps(nextProps) {
+    let { filmTimeList } = nextProps
+    this.setState({
+      filmTimeList: filmTimeList
+    })
+  }
+  spliceByTime(data) {
+    let _arr_obj = {}
+    data.forEach((item, index) => {
+      if(!_arr_obj[item.releaseTime]) {
+        _arr_obj[item.releaseTime] = []
+        _arr_obj[item.releaseTime].push(item)
+      } else {
+        _arr_obj[item.releaseTime].push(item)
+      }
+    })
+    return _arr_obj
+  }
+	render() {
+    let { filmTimeList } = this.state
+    let listResult = this.spliceByTime(filmTimeList.result)
+    let filmTimeListItem = Object.keys(listResult).map((item, index) => {
+      let oneDayList = listResult[item].map((obj, i) => {
+				let type
+				if(obj.dimensional === 0) type = '2D'
+				if(obj.dimensional === 1) type = '3D'
+				if(obj.dimensional === 2) type = 'IMAX'
+        return (
+          <View key={ 'oneDayList' + i } style={ styles.wrapper }>
+            <View style={ styles.wrapperItem }>
+              <View><Text style={ styles.title }>{obj.showStartTime.split(' ')[1]}</Text></View>
+              <View><Text style={ styles.littleSub }>{obj.duration}</Text></View>
+            </View>
+            <View style={ styles.wrapperItem }>
+              <View><Text style={ styles.title }>{obj.language}{type}</Text></View>
+              <View><Text style={ styles.littleSub }>{obj.hallName}</Text></View>
+            </View>
+            <View style={ styles.wrapperItem }>
+              <View><Text style={ styles.title }>￥{obj.salePrice}</Text></View>
+              <View><Text style={ styles.littleSub }>影城价格￥{obj.standardPrice}</Text></View>
+            </View>
+            <View style={ styles.wrapperItemBuy }>
+              <TouchableOpacity>
+								<Text style={ styles.buyTicket }>购票</Text>
+							</TouchableOpacity>
+            </View>
+          </View>
+        )
+      })
+      return (
+        <ScrollView key={ 'filmTimeListItem' + index }>{ oneDayList }</ScrollView>
+      )
+    })
+			return (
+        <View style={ styles.schemaView }>
+					<ScrollableTabView >
+						{ filmTimeListItem }
+					</ScrollableTabView>
+				</View>
+			)
+		}
+	}
+
+
+const styles = StyleSheet.create({
+	schemaView: {
+		// backgroundColor:'yellow',
+		height: pxTodp(500),
+		position:'absolute',
+		bottom: pxTodp(0)
+	},
+	wrapper: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingLeft: pxTodp(25),
+		paddingRight: pxTodp(25),
+		paddingTop: pxTodp(25),
+		paddingBottom: pxTodp(25),
+		borderBottomWidth: pxTodp(1),
+		borderColor: '#CCCCCC',
+	},
+	wrapperItem: {
+		alignItems: 'center'
+	},
+	littleSub: {
+		color: 'gray',
+		fontSize: pxTodp(24)
+	},
+	wrapperItemBuy: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	buyTicket: {
+		color: '#fe4b37',
+		borderWidth: pxTodp(1),
+		borderColor: '#fe4b37',
+		borderRadius: pxTodp(25),
+		paddingLeft: pxTodp(35),
+		paddingRight: pxTodp(30),
+		paddingTop: pxTodp(10),
+		paddingBottom: pxTodp(10),
+	}
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilmMe)
--- js/Container/FilmMe.js
+++ js/Container/FilmMe.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { mapStateToProps, mapDispatchToProps } from '../Redux/Store/Store'
 import { connect, Provider } from 'react-redux'
 import { StyleSheet, View, Image, Text, WebView } from "react-native"
-
+import pxTodp from '../utils/pxTodp'
+import FilmSchemaList from '../Components/FilmSchemaList'
+var ScrollableTabView = require('react-native-scrollable-tab-view');
 class FilmMe extends Component {
 	static navigationOptions = {
     tabBarLabel: '我的',
@@ -16,11 +18,112 @@ class FilmMe extends Component {
 	componentWillMount() {}
 	componentDidMount() {}
 	render() {
+		let filmTimeList = { "code":0,
+		  "message":"OK",
+		  "result":[{
+		      "hallName": "0号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-23",
+		      "showStartTime": "2016-01-23 18:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": true,
+		      "language": "国语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "0号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-23",
+		      "showStartTime": "2016-01-23 18:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": true,
+		      "language": "国语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "0号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-23",
+		      "showStartTime": "2016-01-23 18:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": true,
+		      "language": "国语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "0号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-23",
+		      "showStartTime": "2016-01-23 18:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": true,
+		      "language": "国语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "1号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-23",
+		      "showStartTime": "2016-01-23 08:40:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": false,
+		      "language": "英语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "2号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-24",
+		      "showStartTime": "2016-01-24 07:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": false,
+		      "language": "国语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "3号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-25",
+		      "showStartTime": "2016-01-25 15:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": true,
+		      "language": "国语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "3号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-26",
+		      "showStartTime": "2016-01-25 13:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": true,
+		      "language": "国语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "3号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-26",
+		      "showStartTime": "2016-01-25 15:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": true,
+		      "language": "国语",
+		      "dimensional": 0
+		    },{
+		      "hallName": "3号厅",
+		      "duration": 100,
+		      "releaseTime": "2016-01-26",
+		      "showStartTime": "2016-01-25 10:30:00",
+		      "standardPrice": 60,
+		      "salePrice": 26,
+		      "canBuy": true,
+		      "language": "国语",
+		      "dimensional": 0
+		    }]
+		}
 			return (
-				<View style = { {flex: 1} }>
-					<WebView
-						source={{uri: 'https://www.baidu.com'}}/>
-				</View>
+				<FilmSchemaList filmTimeList = { filmTimeList }/>
 			)
 		}
 	}
