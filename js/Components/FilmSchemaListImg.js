a5f28a18793ec25b7f8378e6f2bdda0b6c5d041b
An <2103887953@qq.com>
2017-05-08 18:22:58 +0800

    update it

--- js/Components/FilmSchemaListImg.js
+++ js/Components/FilmSchemaListImg.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Image, Text, WebView, TouchableOpacity, ScrollView } from "react-native"
 import pxTodp from '../utils/pxTodp'
+import Swiper from 'react-native-swiper';
 class FilmSchemaListImg extends Component {
 	render() {
 			return (
@@ -10,8 +11,35 @@ class FilmSchemaListImg extends Component {
 	}
 
 
-  const styles = StyleSheet.create({
-  });
+  var styles = StyleSheet.create({
+    wrapper: {
+      backgroundColor: 'yellow',
+      width: pxTodp(400)
+    },
+    slide1: {
+      width: pxTodp(187.5),
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#9DD6EB',
+    },
+    slide2: {
+      width: pxTodp(187.5),
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#97CAE5',
+    },
+    slide3: {
+      width: pxTodp(187.5),
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#92BBD9',
+    },
+    text: {
+      color: '#fff',
+      fontSize: 30,
+      fontWeight: 'bold',
+    }
+  })
 
 
 export default FilmSchemaListImg
